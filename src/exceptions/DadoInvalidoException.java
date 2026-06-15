1958eeafb7923b255d4c9aa4abee8e35ae67718f
kauankutzki <127948465+kauankutzki@users.noreply.github.com>
2026-06-15 18:07:38 -0300

    Update DadoInvalidoException.java

--- src/exceptions/DadoInvalidoException.java
+++ src/exceptions/DadoInvalidoException.java
@@ -1,9 +1,5 @@
 package exceptions;
 
-/**
- * Lancada quando um dado informado nao passa na validacao de negocio.
- * Exemplo: salario negativo, nome vazio.
- */
 public class DadoInvalidoException extends Exception {
 
     public DadoInvalidoException(String mensagem) {
